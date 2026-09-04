1e11df012732cd1b0f840826cc6a02d18f2661c0
wiotemtech <wiotemtech@gmail.com>
2026-09-04 14:22:45 +0300

    Set trial mobile money defaults

--- backend/config/services.php
+++ backend/config/services.php
@@ -2,8 +2,8 @@
 return [
  'momo' => [
   'provider' => env('MOMO_PROVIDER',''),
-  'merchant_code' => env('MOMO_MERCHANT_CODE',''),
-  'ussd_code' => env('MOMO_USSD_CODE',''),
+  'merchant_code' => env('MOMO_MERCHANT_CODE','9948612'),
+  'ussd_code' => env('MOMO_USSD_CODE','*165*3#'),
    'base_url' => env('MOMO_BASE_URL',''),
    'api_key' => env('MOMO_API_KEY',''),
    'api_secret' => env('MOMO_API_SECRET',''),
